ed5a65454e9ea9b62170daede4d17a884958d13f
codekoya <angelanikolova218@gmail.com>
2022-03-20 19:03:49 +0100

    Added disolve lifecycle of plastic bottle, for Level 5

--- src/components/levels/LevelFive.vue
+++ src/components/levels/LevelFive.vue
@@ -1,16 +1,69 @@
 <template>
-    <section></section>
+    <section>
+        <aside>
+            <img src="../../assets/plastic-bottle.png" :alt="item.alt_text">
+        </aside>
+        <aside>
+            <p v-if="shouldShowText"> The {{ item.name }} will disolve in {{ item.time_to_disolve }} </p>
+            <button @click="showMeTheText" :class="{'active' : shouldShowText}"> {{ item.button_text }} </button>
+        </aside>
+    </section>
 </template>
 
 <script>
 export default {
-    name: 'LevelFive'
+    name: 'LevelFive',
+
+    data() {
+        return {
+            item: {
+                name: 'Plastic Bottle',
+                time_to_disolve: '450 years',
+                alt_text: 'Plastic Bottle',
+                button_text: 'Reveal Time to Disolve'
+            },
+            
+            shouldShowText: false,
+        }
+    },
+    methods: {
+        showMeTheText() {
+            this.shouldShowText = !this.shouldShowText;
+            if(this.shouldShowText == true ){
+                this.item.button_text = 'Hide time to disolve'
+            }
+            else {
+                this.item.button_text = 'Reveal time to disolve'
+            }
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
     section {
+        display: flex;
+        justify-content: center;
+        align-items: center;
         height: 500px;
         background: #2A869D;
     }
+    aside {
+            img {
+                height: 100px;
+                margin-right: 50px;
+            }
+
+            button {
+                border: none;
+                padding: 5px 10px;
+                background: white;
+                color: darkslategrey;
+            }
+
+            button.active {
+                background-color: grey;
+                color: white;
+            }
+        }
 </style>
